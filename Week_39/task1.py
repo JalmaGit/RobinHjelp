5ad11b0a2884f3e3b5fafc05cb7c6ef474bbbd35
JalmaGit <jmbb00@hotmail.com>
2023-09-29 16:32:27 +0200

    Task 1 Complete

--- Week_39/task1.py
+++ Week_39/task1.py
@@ -1,1 +1,14 @@
+import shutil as sh
 
+def simpleSolution():
+    totaldiskSpace = float(input("Write in the size of your drive: "))
+    utilizedDiskSpace = float(input("Write in the current drive usage: "))
+
+    freeDiskSpace = totaldiskSpace - utilizedDiskSpace
+
+    print(f"You have currently {freeDiskSpace} GB of free diskspace ")
+
+def harderSolution():
+    path = str(input("Write in the root to your system, for windows it will be C:\ "))
+    stat = sh.disk_usage(path)
+    print(f"Disk Usage statistics:\n{stat}")
---
+++ main.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.append("Week_39")
+from Week_39 import task1
+
